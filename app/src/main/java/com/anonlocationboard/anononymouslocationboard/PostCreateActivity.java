72a770e8dd0960b244f31ec1bed9b6118983f543
Henry J Schmale <hschmale16@gmail.com>
2017-08-24 17:59:35 -0400

    Init Commit

---
+++ app/src/main/java/com/anonlocationboard/anononymouslocationboard/PostCreateActivity.java
@@ -0,0 +1,18 @@
+package com.anonlocationboard.anononymouslocationboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PostCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_create);
+    }
+
+    public void createPost(View view) {
+
+    }
+}
