b634ef02f28327ab5cf77fc379a43ba5ce624a9f
Henry J Schmale <hschmale16@gmail.com>
2017-08-26 23:30:23 -0400

    Fiddle with the view

--- app/src/main/java/com/anonlocationboard/anononymouslocationboard/PostListViewActivity.java
+++ app/src/main/java/com/anonlocationboard/anononymouslocationboard/PostListViewActivity.java
@@ -8,18 +8,13 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
-import android.widget.Toast;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.anonlocationboard.anononymouslocationboard.API.ApiRequestBuilder;
-import com.anonlocationboard.anononymouslocationboard.API.Post;
 import com.anonlocationboard.anononymouslocationboard.API.PostListing;
 import com.anonlocationboard.anononymouslocationboard.Adapters.PostListingAdapter;
 
@@ -61,7 +56,7 @@ public class PostListViewActivity extends AppCompatActivity {
 
         loadNextDataFromApi(0);
 
-        lvItems.setOnScrollListener(new EndlessScrollListener() {
+        lvItems.setOnScrollListener(new EndlessScrollListener(5, 1) {
             @Override
             public boolean onLoadMore(int page, int totalItemsCount) {
                 loadNextDataFromApi(page);
